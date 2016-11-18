6ce7cfbc8538cc1caaf93d733fcc7c861ad24653
madmarks <bratmiro@gmail.com>
2016-11-18 17:03:26 +0200

    connection string for cloud-hosted MongoDB

--- config/constants.js
+++ config/constants.js
@@ -1,5 +1,6 @@
 module.exports = {
-    connectionString: "mongodb://localhost/moviesDb",
+    // connectionString: "mongodb://localhost/moviesDb",
+    connectionString: "mongodb://miro:1qaz2wsx@ds155747.mlab.com:55747/appharbor_g3qf6g6v",
     genres: ["action", "sci-fi", "fantasy", "horror", "comedy"],
-    pagesCount: 50
+    pagesCount: 10
 };
